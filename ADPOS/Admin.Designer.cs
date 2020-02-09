da9b0d3f43b81aad70c22b157e419b80a87b2c11
mohamedanushkar <mohamedanushkar@gmail.com>
2020-02-09 10:14:07 +0530

    UC inventory completed

--- ADPOS/Admin.Designer.cs
+++ ADPOS/Admin.Designer.cs
@@ -198,6 +198,7 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "Stock In";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // panel2
             // 
